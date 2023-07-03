c2890fc95720c0a45743d4744ec01b9d316f9bc9
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2023-07-03 13:50:48 +0800

    add springboot module

---
+++ squirrel-factory/src/main/java/com/uch/SquirrelFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.uch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquirrelFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquirrelFactoryApplication.class, args);
+    }
+
+}
